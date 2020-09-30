6fae5ed04411864dd02e1720afd0a8361dfb4d9b
unmu <58108391+unmu@users.noreply.github.com>
2020-09-30 22:05:11 +0800

    2020/9/30

---
+++ C++/ALGO/76 - 十进制数转八进制数.cpp
@@ -0,0 +1,15 @@
+编写函数把一个十进制数输出其对应的八进制数。
+样例输入
+9274
+样例输出
+22072
+
+---------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+	int n;
+	scanf("%d", &n);
+	printf("%o", n);
+	return 0;
+}
